b9c4f9fc1808932087acd467b340882e7a3b5773
EagleOfFire <maxoucrafteur83@gmail.com>
2024-06-10 11:33:13 +0200

    Daily challenge of 10/06 and problem 648 (for whatever reason)

---
+++ 1051. Height Checker/1051.HeightChecker.py
@@ -0,0 +1,12 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        ans = 0
+        expectedHeights = []
+        expectedHeights[:] = heights[:]
+        expectedHeights.sort()
+        if expectedHeights == heights:
+            return 0
+        for i in range(len(heights)):
+            if heights[i] != expectedHeights[i]:
+                ans += 1
+        return ans
